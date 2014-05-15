bb59ed1489d30bf15a941008d3107602edcedf39
user <user@user-desktop.(none)>
2014-05-15 21:55:23 +0530

    added changes

---
+++ newspree/test/spreetest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spreetest::Application.config.secret_key_base = '0b3e0b65e2c7441f4b78277d97de7906a984b7014a7a29275b3b86035313ad0efe85ad3091c7e49b81729e22c6dd64f60e94595174dfd1db16bc78e374927571'
---
+++ newspree/test/spreetest/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spreetest::Application.config.session_store :cookie_store, key: '_spreetest_session'
